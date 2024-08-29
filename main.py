57b1edad25b1b71581f21e88292cd2702c0b5fb4
MariosMagas <mariosmorait.01@gmail.com>
2024-08-29 17:22:43 +0300

    main

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello World')
+import pikepdf
+from pikepdf import PasswordError
+
+def unlock_and_save(original_pdf, password):
+    unlocked_pdf = 'unlocked.pdf'
+    try:
+        with pikepdf.open(original_pdf, password=password) as pdf:
+            pdf.save(unlocked_pdf)
+            print('OK!!!')
+    except PasswordError:
+        print('Wrong Password!!!')
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+unlock_and_save(original_pdf='1.pdf', password='maris')
