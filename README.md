7796e26383b40decb5d5ed0a41b0588788ff82f2
MariosMagas <mariosmorait.01@gmail.com>
2024-08-30 13:19:38 +0300

    1st release completed

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PDF-Decryption
 This is an App that allows you to save your password-protected PDFs as a decrypted copy, so don't need to remember your PDF's password all your life :)
-test
+
+Modules needed for using the source code
+- pip install tkinter
+- pip install customtkinter 
+- pip install pikepdf
--- pdf_store_unlock.py
+++ pdf_store_unlock.py
@@ -1,7 +1,24 @@
 from tkinter import *
 from tkinter import filedialog
 import customtkinter as ctk
-from pdf_unlock import main
+import pikepdf
+
+def algorithm(path, password):
+    if path == '':
+        result = 'No file selected'
+        return result
+    
+    new_file = f'{path[:-4]}_unlocked'
+    try:
+        with pikepdf.open(path, password=password) as pdf:
+            pdf.save(f'{new_file}.pdf')
+    except Exception as e:
+       _removed_path = str(e).split(': ') # e = python file path + error msg. Display of the entire path would force the app window to become unnecessarily wide. 
+       result = f'Error : {_removed_path[1]}'
+       return result
+
+    result = 'Hooray!!!!!!'
+    return result
 
 def path_button_click():
     root.file_path = filedialog.askopenfilename(filetypes=[('PDF Files', '*.pdf')])
@@ -10,7 +27,7 @@ def path_button_click():
         path_label.configure(text='Selected file: ' + file_name[-1])
     
 def convert_button_click():
-   result = main(path=root.file_path, password=table_entry.get())
+   result = algorithm(path=root.file_path, password=table_entry.get())
    result_label.configure(text=result)
 
 # MAIN WINDOW
@@ -38,6 +55,6 @@ convert_button = ctk.CTkButton(master=root, text='Unlock',command=convert_button
 convert_button.place(relx=0.3, y=160, anchor=NW)
 
 result_label = ctk.CTkLabel(master=root,text='',font=('Arial',15),bg_color='transparent')   
-result_label.place(relx = 0.3, y = 200, anchor=NW)
+result_label.place(relx = 0.35, y = 200, anchor=NW)
 
 root.mainloop()
